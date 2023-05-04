b226c352240d26391c6244885a8dc69b3a230b0c
parkgs7599 <98008894+parkgs7599@users.noreply.github.com>
2023-05-04 19:05:48 +0900

    Add files via upload

---
+++ gpt_test/src/main/java/cc/vayne/controller/Controller.java
@@ -0,0 +1,48 @@
+package cc.vayne.controller;
+
+import cc.vayne.dto.ResponseModel;
+import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
+import io.github.flashvayne.chatgpt.service.ChatgptService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+@Slf4j
+@CrossOrigin
+@RestController
+public class Controller {
+
+    @Autowired
+    private ChatgptService chatgptService;
+
+    @GetMapping("/send")
+    public ResponseModel<String> send(HttpServletRequest request, @RequestParam String message) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("requestId {}, ip {}, send a message : {}", requestId, request.getRemoteHost(), message);
+        if (!StringUtils.hasText(message)) {
+            return ResponseModel.fail("message can not be blank");
+        }
+        String responseMessage = chatgptService.sendMessage(message);
+        log.info("requestId {}, ip {}, get a reply : {}", requestId, request.getRemoteHost(), responseMessage);
+        return ResponseModel.success(responseMessage);
+    }
+
+    @PostMapping("/multi/send")
+    public ResponseModel<String> multiSend(HttpServletRequest request, @RequestBody List<MultiChatMessage> messages) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("requestId {}, ip {}, send messages : {}", requestId, request.getRemoteHost(), messages.toString());
+        if (CollectionUtils.isEmpty(messages)) {
+            return ResponseModel.fail("messages can not be empty");
+        }
+        String responseMessage = chatgptService.multiChat(messages);
+        log.info("requestId {}, ip {}, get a reply : {}", requestId, request.getRemoteHost(), responseMessage);
+        return ResponseModel.success(responseMessage);
+    }
+
+
+}
