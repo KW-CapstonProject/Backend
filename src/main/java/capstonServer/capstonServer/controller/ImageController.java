e98c20e1921eac2f950e734c4f56af44241b27dc
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2023-05-18 21:23:40 +0900

    fix/study member

--- src/main/java/capstonServer/capstonServer/controller/ImageController.java
+++ src/main/java/capstonServer/capstonServer/controller/ImageController.java
@@ -1,61 +1,87 @@
 package capstonServer.capstonServer.controller;
 
 
-
+import ai.djl.MalformedModelException;
 import ai.djl.Model;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
-
+import ai.djl.modality.Classifications.Classification;
 import ai.djl.modality.cv.ImageFactory;
 import ai.djl.ndarray.NDArray;
-import java.awt.image.BufferedImage;
 import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.types.DataType;
+import ai.djl.translate.TranslateException;
 import ai.djl.translate.Translator;
 import ai.djl.translate.TranslatorContext;
-import ai.djl.translate.TranslateException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Paths;
-import javax.imageio.ImageIO;
-@RestController
-@RequestMapping("/api/image")
+
+@Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/api/image")
+@RestController
+
 public class ImageController {
 
-    private static final String MODEL_PATH = "./gogh2photo_G_BA.pt";
-    private static final String IMAGE_PATH = "./tree.jpeg";
-    private static final int TARGET_WIDTH = 64;
-    private static final int TARGET_HEIGHT = 64;
-
     @GetMapping("")
-    public void runStyleTransfer() {
-        try (Model model = Model.newInstance(MODEL_PATH)) {
-            // Create a Translator for resizing and processing the input image
-            Translator<BufferedImage, Classifications> translator = new ImageTranslator(TARGET_WIDTH, TARGET_HEIGHT);
+    public void run () {
+        String modelPath = "models/gogh2photo_G_BA.pt";
+        String imagePath = "images/tree.jpeg";
+        int targetWidth = 64;
+        int targetHeight = 64;
+
+        // Load the PyTorch model
+        try (Model model = Model.newInstance("my_model")) {
+
+            model.load(Paths.get(modelPath));
+            // Create a Translator for resizing the input image
+            Translator<BufferedImage, Classifications> translator = new ImageTranslator(targetWidth, targetHeight);
 
             // Create a Predictor for inference
             try (Predictor<BufferedImage, Classifications> predictor = model.newPredictor(translator)) {
+
                 // Load and resize the input image
-                BufferedImage image = loadImage(IMAGE_PATH);
-                Classifications result = predictor.predict(image);
+                BufferedImage image = loadImage(imagePath);
+                System.out.println(image);
+                BufferedImage resizedImage = resizeImage(image, targetWidth, targetHeight);
+                System.out.println(resizedImage);
+                // Perform inference
+                Classifications result = predictor.predict(resizedImage);
+                System.out.println(result);
+
 
                 // Process the output
-                for (Classifications.Classification classification : result.items()) {
+                for (Classification classification : result.items()) {
                     System.out.println(classification.getClassName() + ": " + classification.getProbability());
                 }
             }
-        } catch (IOException | TranslateException e) {
+        } catch (IOException  | TranslateException e) {
             e.printStackTrace();
+        } catch (MalformedModelException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private static BufferedImage loadImage(String imagePath) throws IOException {
         return ImageIO.read(Paths.get(imagePath).toFile());
+    }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.drawImage(originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH), 0, 0, targetWidth, targetHeight, null);
+        g2d.dispose();
+        return resizedImage;
     }
 
     private static class ImageTranslator implements Translator<BufferedImage, Classifications> {
@@ -77,7 +103,6 @@ public class ImageController {
 
             return new NDList(array);
         }
-
         @Override
         public Classifications processOutput(TranslatorContext ctx, NDList list) {
             // Process the output
