0ce2095df090ff2b7dcda20aaca28d64409153bc
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2023-08-03 13:46:45 +0900

    chore/test code delete

--- src/test/java/com/pocekt/art/ArtApplicationTests.java
+++ src/test/java/com/pocekt/art/ArtApplicationTests.java
@@ -1,9 +1,8 @@
 package com.pocekt.art;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ArtApplicationTests {
 
 	@Test
