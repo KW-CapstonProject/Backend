248a2efa5f336fd4478061b977e76d882b27acbf
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2023-08-03 13:44:35 +0900

    feat/add property

--- src/main/java/com/pocekt/art/config/S3config.java
+++ src/main/java/com/pocekt/art/config/S3config.java
@@ -10,8 +10,10 @@ import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class S3config {
     @Value("${app.openai.api}")
     private String apiKey;
